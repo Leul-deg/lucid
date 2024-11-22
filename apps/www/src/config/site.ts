2a754d0bf0fe17f41e98b2d4a856f51f9bf9fe84
Leul-deg <leuldegarege11@gmail.com>
2024-11-22 13:15:55 +0300

    feat(www): add static brand assets and public images

---
+++ apps/www/src/config/site.ts
@@ -0,0 +1,12 @@
+import { SiteConfig } from "types";
+
+export const siteConfig: SiteConfig = {
+    name: "LUCID",
+    description: "Privacy first. Open source. Web analytics.",
+    url: "https://www.lucid.io",
+    ogImage: "https://www.lucid.io/og.png",
+    links: {
+        twitter: "https://twitter.com/lucid_io",
+        github: "https://github.com/lucid/lucid",
+    },
+};
